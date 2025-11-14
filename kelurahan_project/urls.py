2791bfd9dcb69bbd511adff1da06fdd7012b5b12
yudi010306 <ezyudi68@gmail.com>
2025-11-14 21:16:45 +0700

    Praktikum 7: Update API Warga CRUD dengan ModelViewSet & Router

--- kelurahan_project/urls.py
+++ kelurahan_project/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
 
-    # HTML Web
-    path('', lambda request: redirect('warga_list')),
-    path('warga/', include('warga.urls')),
+    # Halaman web
+    path('', lambda request: redirect('warga_list')),  # Redirect root ke daftar warga
+    path('warga/', include('warga.urls')),           # URL untuk web page
 
-    # API Modern (ViewSet + Router)
+    # API Modern (ViewSet + Router) - praktikum 7
     path('api/', include('warga.api_urls')),
 
-    # API Praktikum 6 (ListAPIView + DetailAPIView)
+    # API Lama (ListAPIView + DetailAPIView) - praktikum 6, tetap disimpan
     path('api/v1/', include('warga.api_urls_p6')),
 ]
